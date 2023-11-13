3eb8bae03e1ba621ac6f53dd5c174934109dc32b
Michael Housh <mhoush@houshhomeenergy.com>
2023-11-13 09:20:08 -0500

    feat: adds zitadel configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,41 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    // register routes
-    try routes(app)
+  // register routes
+  try routes(app)
+  app.zitadelConfiguration = try ZitadelConfiguration()
+}
+
+struct ZitadelConfigurationError: Error {}
+
+struct ZitadelConfiguration {
+  var domain: String
+  var clientId: String
+  var clientSecret: String
+
+  init() throws {
+    guard let domain = Environment.get("ZITADEL_DOMAIN"),
+      let clientId = Environment.get("CLIENT_ID"),
+      let clientSecret = Environment.get("CLIENT_SECRET")
+    else {
+      throw ZitadelConfigurationError()
+    }
+    self.domain = domain
+    self.clientId = clientId
+    self.clientSecret = clientSecret
+  }
+}
+
+struct ZitadelConfigurationKey: StorageKey {
+  typealias Value = ZitadelConfiguration
+}
+
+extension Application {
+  var zitadelConfiguration: ZitadelConfiguration? {
+    get { self.storage[ZitadelConfigurationKey.self] }
+    set { self.storage[ZitadelConfigurationKey.self] = newValue }
+  }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,27 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+  // Remove
+  app.get("config") { req async throws -> String in
+    guard let config = req.application.zitadelConfiguration else {
+      throw Abort(.notFound)
     }
+    req.logger.info("Zitadel Domain: \(config.domain)")
+    req.logger.info("Client ID: \(config.clientId)")
+    req.logger.info("Client Secret: \(config.clientSecret)")
+    return config.domain
+  }
+
+  app.get("api", "public") { req async -> String in
+    "Public route - You don't need to be authenticated to see this."
+  }
+
+  app.get("api", "private") { req async -> String in
+    "Private route - You need to be authenticated to see this."
+  }
+
+  app.get("api", "private-scoped") { req async -> String in
+    "Private, scoped route - You need to be authenticated and have the role read:messages to see this."
+  }
 }
